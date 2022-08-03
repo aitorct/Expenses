7bca31f3c4ee0147d482fcbbd44d5a28acd6edab
Aitor Cubeles Torres <aitor@factorial.co>
2022-08-04 00:08:06 +0200

    Add Spanish localization

---
+++ src/locales/es.ts
@@ -0,0 +1,39 @@
+export default {
+  expense: {},
+  camera: {
+    permission_denied: 'El permiso de acceso a la cámara ha sido denegado',
+  },
+  item: {
+    add_receipt: 'Añadir recibo',
+    check_receipt: 'Ver recibo',
+  },
+  filters: {
+    title: 'Filtros',
+    user: {
+      title: 'Usuario',
+      placeholder: 'Selecciona un usuario...',
+      selection: {
+        placeholder: 'Escribe un nombre...',
+        emptySearch: 'No hay usuarios que coincidan con la búsqueda',
+      },
+    },
+    userSelection: {
+      title: 'Usuario',
+    },
+    year: {
+      title: 'Año',
+    },
+    receipts: {
+      title: 'Recibos',
+      attached: 'Adjuntado',
+      unattached: 'No adjuntado',
+    },
+  },
+  user_selection: {
+    title: 'Selección de usuario',
+  },
+  share: {
+    on: 'el',
+    spent_by: 'Gastado por',
+  },
+};
